15ee06027dcbeea9dd1f454610b6598ad063bce1
shakthivel <shakthivels94@gmail.com>
2020-09-18 20:40:19 -0400

    Actor Movie Relationship Model and GET APIs

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import os
 import sys
 import json
+import datetime
 from flask import Flask, request, Response, abort, jsonify
 from werkzeug.datastructures import MultiDict
-from models import setup_db, Actor
+from models import setup_db, db_drop_and_create_all, Actor, Movie, CastedIn
 from flask_cors import CORS, cross_origin
 
 
@@ -13,17 +14,51 @@ def create_app(test_config=None):
     setup_db(app)
     cors = CORS(app)
 
+    db_drop_and_create_all()
+
+    a1 = Actor(name="A", age=30, gender="Male")
+    a1.insert()
+
+    m1 = Movie(title="M", release_date=datetime.datetime(2020, 9, 18))
+    m1.insert()
+
+    c1 = CastedIn(actor_id=1, movie_id=1)
+    c1.insert()
+
     @app.route('/')
     def index():
-        return jsonify("Hello World!")
+        return jsonify("Healthy!")
 
-    @app.route('/actors/<id>')
-    def get_actors(id):
-        result = Actor.query.filter(Actor.id == id).first()
-        if result:
-            return jsonify(result.format())
-        else:
-            abort(404)
+    @app.route('/actors', methods=["GET"])
+    def get_actors():
+
+        query_result = Actor.query.all()
+        actors = []
+
+        for obj in query_result:
+            actor = obj.format()
+            movies = []
+            for casted_in in obj.casted_in:
+                movies.append(casted_in.movie.title)
+            actor["movies"] = movies
+            actors.append(actor)
+
+        return jsonify(actors)
+
+    @app.route('/movies', methods=["GET"])
+    def get_movies():
+        query_result = Movie.query.all()
+        movies = []
+
+        for obj in query_result:
+            movie = obj.format()
+            actors = []
+            for casted in obj.casted:
+                actors.append(casted.actor.name)
+            movie["actors"] = actors
+            movies.append(movie)
+
+        return jsonify(movies)
 
     return app
 
--- models.py
+++ models.py
@@ -8,6 +8,11 @@ database_path = os.environ['DATABASE_URL']
 db = SQLAlchemy()
 
 
+def db_drop_and_create_all():
+    db.drop_all()
+    db.create_all()
+
+
 '''
 setup_db(app)
     binds a flask application and a SQLAlchemy service
@@ -22,10 +27,25 @@ def setup_db(app, database_path=database_path):
     db.create_all()
 
 
-'''
-Actor
-Have name, age and gender
-'''
+class CastedIn(db.Model):
+    __tablename__ = 'casted_in'
+    actor_id = Column(db.Integer, db.ForeignKey(
+        'actor.id'), primary_key=True)
+    movie_id = Column(db.Integer, db.ForeignKey(
+        'movie.id'), primary_key=True)
+    actor = db.relationship("Actor", back_populates="casted_in")
+    movie = db.relationship("Movie", back_populates="casted")
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
 
 
 class Actor(db.Model):
@@ -33,11 +53,19 @@ class Actor(db.Model):
 
     id = Column(db.Integer, primary_key=True)
     name = Column(db.String)
-    age = Column(db.String)
+    age = Column(db.Integer)
     gender = Column(db.String)
+    casted_in = db.relationship('CastedIn', back_populates='actor')
 
     def insert(self):
         db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
         db.session.commit()
 
     def format(self):
@@ -46,3 +74,29 @@ class Actor(db.Model):
             'name': self.name,
             'age': self.age,
             'gender': self.gender}
+
+
+class Movie(db.Model):
+    __tablename__ = 'movie'
+
+    id = Column(db.Integer, primary_key=True)
+    title = Column(db.String)
+    release_date = db.Column(db.DateTime())
+    casted = db.relationship('CastedIn', back_populates='movie')
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release_date': self.release_date}
