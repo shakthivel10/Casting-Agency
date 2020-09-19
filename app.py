d4ae9014a438d3fd3420c879454c733d1bd01a63
shakthivel <shakthivels94@gmail.com>
2020-09-18 20:46:37 -0400

    removed drop_db used for testing

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def create_app(test_config=None):
     setup_db(app)
     cors = CORS(app)
 
-    db_drop_and_create_all()
+    # db_drop_and_create_all()
 
     a1 = Actor(name="A", age=30, gender="Male")
     a1.insert()
